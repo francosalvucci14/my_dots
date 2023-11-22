d320f1df82c2db28fb8094873316cdee77848a9c
francosalvucci14 <francosalvucci14@gmail.com>
2023-11-22 11:10:29 +0100

    Update python formatter

--- .config/nvim/lua/custom-keys.lua
+++ .config/nvim/lua/custom-keys.lua
@@ -7,10 +7,7 @@ vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Sav
 -- update leader key to ,
 -- vim.keymap.set("n", "<leader>ot", function() require("trouble").open() end)
 -- vim.keymap.set("n", "<leader>ct", function() require("trouble").close() end)
-vim.keymap.set("n", "<C-Up>",[[:Trouble<CR>]],{desc = 'Open toggle'})
-vim.keymap.set("n", "<C-Down>",[[:TroubleClose<CR>]],{desc = 'Close toggle'} )
+vim.keymap.set("n", "<C-Up>", [[:Trouble<CR>]], { desc = 'Open toggle' })
+vim.keymap.set("n", "<C-Down>", [[:TroubleClose<CR>]], { desc = 'Close toggle' })
 
 vim.g.mapleader = " "
-
- 
-
