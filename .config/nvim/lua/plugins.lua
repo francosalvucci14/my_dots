7ec0aed7425b3ef256efdd696332644ab22e5d50
francosalvucci14 <francosalvucci14@gmail.com>
2024-03-20 14:17:45 +0100

    Update Plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,6 +13,7 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/vim-vsnip'
+	use "nvim-neotest/nvim-nio"
 	-- DAP for debugging
 	use 'mfussenegger/nvim-dap'
 	use {
